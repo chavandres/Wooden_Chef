934c3b849d783906fbba18647751629dd3f30092
andresator <achavarria70005@ufide.ac.cr>
2021-12-10 23:46:27 -0600

    Thread llenar queue random cada segundo

--- src/classes/test.java
+++ src/classes/test.java
@@ -12,10 +12,10 @@ public class test {
         Player player = new Player();
 
         
-        Ingrediente pan=new Ingrediente("Pan","image");
-        Ingrediente carne=new Ingrediente("Carne","image");
-        Ingrediente queso=new Ingrediente("Queso","image");
-        Ingrediente lechuga=new Ingrediente("Lechuga","image");
+        Ingrediente pan=new Ingrediente("Pan","/resources/pan.png");
+        Ingrediente carne=new Ingrediente("Carne","/resources/carne.png");
+        Ingrediente queso=new Ingrediente("Queso","/resources/queso.png");
+        Ingrediente lechuga=new Ingrediente("Lechuga","/resources/lechuga.png");
         LinkedList<Ingrediente> listaSencilla=new LinkedList<>();
         listaSencilla.insert(pan);
         listaSencilla.insert(carne);
@@ -31,9 +31,9 @@ public class test {
         listaClasica.insert(queso);
         listaClasica.insert(lechuga);
 
-        Orden hamSencilla = new Orden(1, "image", listaSencilla);
-        Orden hamQueso = new Orden(2, "image", listaQueso);
-        Orden hamClasica = new Orden(3, "image", listaClasica);
+        Orden hamSencilla = new Orden(1, "/resources/hamconcarne.png", listaSencilla);
+        Orden hamQueso = new Orden(2, "/resources/hamconqueso.png", listaQueso);
+        Orden hamClasica = new Orden(3, "/resources/hamclasica.png", listaClasica);
         
         Queue<Orden> ordenes=new Queue<>();
         CircularList<Ingrediente> cinta = new CircularList<>();
@@ -42,8 +42,10 @@ public class test {
         cinta.insert(queso);
         cinta.insert(lechuga);
         cinta.insert(pan);
-        player.getPlatillo().insert(cinta.extract(4));
-                           
+        player.getPlatillo().insert(cinta.extract(4)); //Agregar elemento de la cinta al platillo del jugador
+         
+
+//Thread de llenar el queue de ordenes                  
         Runnable llenarCola = new Runnable() {
             @Override
             public void run() {
@@ -54,9 +56,29 @@ public class test {
                         // En él, hacemos que el hilo duerma
                         Thread.sleep(1000);
                         // Y después realizamos las operaciones
-                        if(ordenes.size<3){
-                            ordenes.add(hamClasica);
-                        } else{
+                        if (ordenes.size < 3) {
+                            int num;
+                            num = (int) (Math.random() * (3) + 1);
+                            if (num == 4) {
+                                num--;
+                            }
+                            switch (num) {
+                                case 1: {
+                                    ordenes.add(hamSencilla);
+                                    break;
+                                }
+                                case 2: {
+                                    ordenes.add(hamQueso);
+                                    break;
+                                }
+                                case 3: {
+                                    ordenes.add(hamClasica);
+                                    break;
+                                }
+                            }
+                        }
+                        
+                        else{
                             System.out.println("Cola llena");
                         }
                         // Así, se da la impresión de que se ejecuta cada cierto tiempo
