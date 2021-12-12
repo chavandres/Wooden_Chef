4bf70b748ee89b85c8ba1f75ec73bd157d619846
Flody27 <diegovv1300@gmail.com>
2021-12-11 21:29:49 -0600

    Metodo get por posicion

--- src/classes/Stack.java
+++ src/classes/Stack.java
@@ -4,12 +4,13 @@ public class Stack<T> {
 
     public Node<T> top = null;
     boolean orden = true;
+    int size = 0;
 
     public void push(T value) {
         Node<T> temp = new Node<>(value);
         temp.setNext(top);
         top = temp;
-
+         size++;
     }
 
     public Node<T> pop() {
@@ -29,5 +30,26 @@ public class Stack<T> {
             System.out.println(actual.getData());
         }
 
+    }
+        public T printPos(int value) {
+
+        if (value == size || value < 0) {
+
+            return null;
+        }
+        Node<T> actual = top;
+        if (actual == null) {
+            System.out.println(".........");
+        } else {
+            if (value == 0) {
+                return actual.getData();
+            }
+            for (int x = 0; x < value; x++) {
+                actual = actual.getNext();
+            }
+
+            return actual.getData();
+        }
+        return null;
     }
 }
